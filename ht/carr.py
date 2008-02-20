02b81cb361bae0f63d9465441d4e8c73c8973a6f
betisman <betisman@7d62d419-8846-0410-93fb-7924d0d160c8>
2008-02-20 10:05:47 +0000

    preparado para el carrusel de hoy

--- ht/carr.py
+++ ht/carr.py
@@ -4,16 +4,8 @@ import urllib, httplib2
 import sys
 import time, datetime
 #httplib2.debuglevel = 1
-http = httplib2.Http()
 
-#variables globales
-recServer = ''
-headers = {'Content-type': 'application/x-www-form-urlencoded'}
-username = 'betisman'
-password = 'logaritmo'
-#hasta aquí, variables globales
-
-def getRecommendedServer():
+def getRecommendedServer(http):
 	try:
 		url = 'http://www.hattrick.org/Common/menu.asp?outputType=XML'
 		#body = {'u_name':username,'p_word':password}
@@ -31,10 +23,11 @@ def getRecommendedServer():
 		print 'Salto una excepcion en getRecommendedServer()'
 		return None
 
-def login():
+def login(username, password, recServer, http):
 	try:
 		userAgent = 'MyApp/v1.0'
 		cookie = ''
+		headers = {'Content-type': 'application/x-www-form-urlencoded'}
 		url = recServer + '/common/default.asp'
 		print url
 		body = {'loginname':username,'password':password,'actionType':'login','flashVersion':'0','submit.x':'0','submit.y':'0','submit':'Entrar'}
@@ -45,7 +38,8 @@ def login():
 		except KeyError:
 			print 'Saltó excepción KeyError'
 			return None
-		return 'Login OK'
+		print 'Login OK'
+		return http, headers
 	except Exception:
 		print 'Saltó una excepción en login().'
 		print 'Info: '
@@ -75,49 +69,62 @@ def getMatches(path):
 	for matchid in matchids:
 		ret.append(matchid.firstChild.nodeValue)
 	return ret
-	
-pathXmls = ".\\xmls\\"
-pathMatchids = pathXmls + "matchids.xml"
-now = datetime.datetime.now()
-strResultados = "RESULTADOS"+ " (" + str(now.hour) + ":" + str(now.minute) + ")\n\n"
 
-recServer = getRecommendedServer()
-login()
-matchids = getMatches(pathMatchids);
-print str(len(matchids)) + ' partidos'
-for matchid in matchids:
-	url = recServer + '/Common/chppxml.axd?file=live&actionType=addMatch&matchid=' + matchid
-	print url
-	try:
-		response, content = http.request(url, 'GET', headers=headers)
-		#afichero(content, pathXmls + 'live'+matchid+'.xml')
-		
-		doc = minidom.parseString(content)
-		hometeam = doc.getElementsByTagName('HomeTeamName')[0].firstChild.nodeValue
-		awayteam = doc.getElementsByTagName('AwayTeamName')[0].firstChild.nodeValue
-		homegoals = doc.getElementsByTagName('HomeGoals')[0].firstChild.nodeValue
-		awaygoals = doc.getElementsByTagName('AwayGoals')[0].firstChild.nodeValue
-		
-		#calculo del minuto actual
-		inicio = doc.getElementsByTagName('MatchDate')[0].firstChild.nodeValue
-		inicio = time.mktime(time.strptime(inicio, "%Y-%m-%d %H:%M:%S"))
-		inicio = datetime.datetime.fromtimestamp(inicio)
-		ahora = datetime.datetime.now()
-		diferencia = ahora-inicio
-		diferencia, segundos = divmod(diferencia.seconds, 60)
-		if (diferencia > 45):
-			if (diferencia > 60):
-				diferencia = diferencia - 15
-				if (diferencia > 90):
-					diferencia = 90	
+def carruselear():
+	http = httplib2.Http()
+	#variables globales
+	recServer = ''
+	#headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	username = 'betisman'
+	password = 'logaritmo'
+	#hasta aquí, variables globales
+	pathXmls = ".\\xmls\\"
+	pathMatchids = pathXmls + "matchids.xml"
+	now = datetime.datetime.now()
+	strResultados = "RESULTADOS"+ " (" + str(now.hour) + ":" + str(now.minute) + ")\n\n"
+
+	recServer = getRecommendedServer(http)
+	http, headers = login(username, password, recServer, http)
+	matchids = getMatches(pathMatchids);
+	print str(len(matchids)) + ' partidos'
+	for matchid in matchids:
+		url = recServer + '/Common/chppxml.axd?file=live&actionType=addMatch&matchid=' + matchid
+		#print url
+		try:
+			response, content = http.request(url, 'GET', headers=headers)
+			#afichero(content, pathXmls + 'live'+matchid+'.xml')
+			
+			
+			doc = minidom.parseString(content)
+			hometeam = doc.getElementsByTagName('HomeTeamName')[0].firstChild.nodeValue
+			awayteam = doc.getElementsByTagName('AwayTeamName')[0].firstChild.nodeValue
+			homegoals = doc.getElementsByTagName('HomeGoals')[0].firstChild.nodeValue
+			awaygoals = doc.getElementsByTagName('AwayGoals')[0].firstChild.nodeValue
+			
+			
+			#calculo del minuto actual
+			inicio = doc.getElementsByTagName('MatchDate')[0].firstChild.nodeValue
+			inicio = time.mktime(time.strptime(inicio, "%Y-%m-%d %H:%M:%S"))
+			inicio = datetime.datetime.fromtimestamp(inicio)
+			ahora = datetime.datetime.now()
+			if ahora < inicio:
+				diferencia = 0
 			else:
-				diferencia = 45
-		minuto = str(diferencia)
-		
-		
-		strResultados = strResultados + hometeam + " " + homegoals + " - " + awaygoals + " " + awayteam + " (minuto " + minuto + ")\n"
-	except Exception, message:
-		print 'joer'
+				diferencia = ahora-inicio
+				diferencia, segundos = divmod(diferencia.seconds, 60)
+				if (diferencia > 45):
+					if (diferencia > 60):
+						diferencia = diferencia - 15
+						if (diferencia > 90):
+							diferencia = 90	
+					else:
+						diferencia = 45
+			minuto = str(diferencia)
+			
+			strResultados = strResultados + hometeam + " " + homegoals + " - " + awaygoals + " " + awayteam + " (minuto " + minuto + ")\n"
+		except Exception, message:
+			print 'No se ha podido tratar el partido', matchid, '\n', sys.exc_info()
+			print message
 
-strResultados = strResultados + '\n\n\nCarrusel automatico v1.0 implementado en carr.py'
-afichero(unicode(strResultados), pathXmls+'carr.txt')
+	strResultados = strResultados + '\n\n\nCarrusel automatico v1.1 implementado en carr.py'
+	afichero(unicode(strResultados), pathXmls+'carr.txt')
--- ht/sendgmail.py
+++ ht/sendgmail.py
@@ -43,30 +43,29 @@ from email import Encoders
 import os
 
 def sendGmail(gmailuser, gmailpwd, to, subject, text, attach):
-   msg = MIMEMultipart()
+	msg = MIMEMultipart()
 
-   msg['From'] = gmailuser
-   msg['To'] = to
-   msg['Subject'] = subject
+	msg['From'] = gmailuser
+	msg['To'] = to
+	msg['Subject'] = subject
 
-   msg.attach(MIMEText(text))
+	msg.attach(MIMEText(text))
 
-   part = MIMEBase('application', 'octet-stream')
-   if attach != None:
-	   part.set_payload(open(attach, 'rb').read())
-	   Encoders.encode_base64(part)
-	   part.add_header('Content-Disposition',
-	           'attachment; filename="%s"' % os.path.basename(attach))
-	   msg.attach(part)
+	part = MIMEBase('application', 'octet-stream')
+	if attach != None:
+		part.set_payload(open(attach, 'rb').read())
+		Encoders.encode_base64(part)
+		part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(attach))
+		msg.attach(part)
 
-   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
-   mailServer.ehlo()
-   mailServer.starttls()
-   mailServer.ehlo()
-   mailServer.login(gmailuser, gmailpwd)
-   mailServer.sendmail(gmailuser, to, msg.as_string())
-   # Should be mailServer.quit(), but that crashes...
-   mailServer.close()
+	mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+	mailServer.ehlo()
+	mailServer.starttls()
+	mailServer.ehlo()
+	mailServer.login(gmailuser, gmailpwd)
+	mailServer.sendmail(gmailuser, to, msg.as_string())
+	# Should be mailServer.quit(), but that crashes...
+	mailServer.close()
 
 def prueba():
 	gmailuser = 'betisman@gmail.com'
@@ -82,4 +81,4 @@ def prueba():
 	except Exception:
 		print 'El mail (creo) no ha sido enviado.'
 
-prueba()
+#prueba()
