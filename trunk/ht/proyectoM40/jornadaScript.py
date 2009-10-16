9429f1ee64497da5d48dfe9efc89f2aa69270719
betisman <betisman@7d62d419-8846-0410-93fb-7924d0d160c8>
2009-10-16 10:52:48 +0000

    no es push, es append

--- trunk/ht/proyectoM40/jornadaScript.py
+++ trunk/ht/proyectoM40/jornadaScript.py
@@ -62,9 +62,9 @@ def crearFicheroResultados(path, matches):
             res['e'] = res['e'] + 1
 
         if enAscenso(partido['liga'], partido['posLiga']):
-            arrAscenso.push(partido['siglas'])
+            arrAscenso.append(partido['siglas'])
         if enDescenso(partido['liga'], partido['posLiga']):
-            arrDescenso.push(partido['siglas'])
+            arrDescenso.append(partido['siglas'])
 
     cadena = cadena + '\n%d jugados: %d ganados, %d empatados, %d perdidos\n' % (len(matches), res['g'], res['e'], res['p'])
     cadena = '%s\n%d equipos en ascenso: %s' % (cadena, len(arrAscenso), strArrayEquipos(arrAscenso))
